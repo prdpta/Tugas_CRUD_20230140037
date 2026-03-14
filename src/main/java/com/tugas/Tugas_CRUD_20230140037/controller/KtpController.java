ddaed8c64d53757dfb9e8df13ba55cfe0e08234d
prdpta <pradiptaputramei01@gmail.com>
2026-03-14 23:36:51 +0700

    implementasi KtpController untuk menyediakan endpoint REST API CRUD

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140037/controller/KtpController.java
@@ -0,0 +1,45 @@
+package com.tugas.Tugas_CRUD_20230140037.controller;
+
+import com.tugas.Tugas_CRUD_20230140037.dto.KtpDto;
+import com.tugas.Tugas_CRUD_20230140037.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ktp") // DISESUAIKAN: Menambahkan /api agar sesuai dengan $.ajax di index.html
+@CrossOrigin
+public class KtpController {
+
+    @Autowired
+    private KtpService ktpService;
+
+    @PostMapping
+    public ResponseEntity<KtpDto> tambahKtp(@RequestBody KtpDto ktpDto) {
+        // Pastikan nama method di service adalah tambahKtp
+        return ResponseEntity.ok(ktpService.tambahKtp(ktpDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<KtpDto>> ambilSemuaKtp() {
+        return ResponseEntity.ok(ktpService.ambilSemuaKtp());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<KtpDto> ambilKtpById(@PathVariable Integer id) {
+        return ResponseEntity.ok(ktpService.ambilKtpById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<KtpDto> updateKtp(@PathVariable Integer id, @RequestBody KtpDto ktpDto) {
+        return ResponseEntity.ok(ktpService.updateKtp(id, ktpDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> hapusKtp(@PathVariable Integer id) {
+        ktpService.hapusKtp(id);
+        return ResponseEntity.noContent().build();
+    }
+}
