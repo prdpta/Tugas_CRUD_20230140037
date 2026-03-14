28ce368eef9f2b05ef25c2dc7794bc5b63b161d7
prdpta <pradiptaputramei01@gmail.com>
2026-03-14 22:04:07 +0700

    Initial commit

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140037/TugasCrud20230140037Application.java
@@ -0,0 +1,13 @@
+package com.tugas.Tugas_CRUD_20230140037;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140037Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140037Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/Tugas_CRUD_20230140037/TugasCrud20230140037ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.Tugas_CRUD_20230140037;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud20230140037ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
